8820478a83c1d769b8d8e471ea799bc9e9b8247a
TheTong25 <tong31973@gmail.com>
2026-03-18 11:59:23 +0700

    rename middleware to proxy

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,6 +2,6 @@ import LoadingUI from "@/components/shared/LoadingUI";
 
 export default function Loading() {
     return <div className="flex justify-center items-center h-screen">
-        <LoadingUI text="Home" />
+        <LoadingUI text="Loading" />
     </div>;
 }
